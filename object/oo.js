322838289072071be4626d4efb7220e5fa7933dd
Daniel Melo <o.danielworkmelo@gmail.com>
2023-01-03 17:46:34 +0000

    initial OO

---
+++ object/oo.js
@@ -0,0 +1,21 @@
+//Procedural
+processamento(valor1, valor2, valor3);//Processamento manipulando dados
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    valor4,
+    processamento(){
+        //...
+    }
+}//objeto com valores, atributos e funções.
+//Objeto funciona como uma cápsula que agrupa comportamentos e atributos;
+
+/*Principios
+Abstração - Entender o contexto de cada objeto e abstrair o que de fato importa
+Encapsulamento -  
+Herança(prototype) -
+Polimorfismo - 
+ */
