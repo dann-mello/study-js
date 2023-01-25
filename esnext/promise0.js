1f936bac8783f18916ff1f791de742c6d07ea51d
Daniel Melo <o.danielworkmelo@gmail.com>
2023-01-25 12:03:15 +0000

    promisses update/1

--- esnext/promise0.js
+++ esnext/promise0.js
@@ -1,17 +1,11 @@
-// function falarDepoisDe(segundos, frase) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             // resolve(frase)
-//             reject(frase)
-//         }, segundos * 1000)
-//     })
-// }
+function falarDepoisDe(segundos,frase){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(frase)
+        },segundos * 1000)
+    })
+}
 
-// //resolve só aceita um parâmetro
-
-// falarDepoisDe(3, 'Que legal')
-//     .then(frase => frase.concat('?!?'))
-//     .then(outraFrase => console.log(outraFrase))
-//     .catch(() => console.log('Deu erro'))
-
-console.log(Promise.prototype)
+falarDepoisDe(3, 'Que legal')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase))
--- esnext/requisicoes.js
+++ esnext/requisicoes.js
@@ -1,12 +1,51 @@
 //https://jsonplaceholder.typicode.com/posts
-function clicou() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+// function clicou() {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then((data) => {
+//             return data.json();//json() -> transforma um json em object
+//         })
+//         .then((data) => {
+//             console.log(data)
+//             alert(`Titulo do primeiro post: ${data[0].title}`)
+//         })
+//         .catch((error) => {
+//             console.log('Deu problema' + error)
+//         })
+//         .finally(() => {
+//             alert('Opa acabou tudo');//Certo ou errado será executado
+//         })
+// }
+async function clicou() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let json = await response.json()
+    alert(`Titulo do primeiro post: ${json[0].title}`);
+    alert('CLICOU');
+}
+
+// document.querySelector('#botao').addEventListener('click', clicou)
+// document.querySelector('#inserir').addEventListener('click', inserir)
+
+function inserir() {
+    fetch('https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: 'Título de teste',
+                body: 'Corpo de teste',
+                userId: 2
+            })
+        }
+    )
         .then((response) => {
-            // console.log(response.body)
-            return response.json()
+            return response.json();
         })
-        .then((data) => {
-            console.log(data)
+        .then((json) => {
+            console.log(json)
+        })
+        .catch((error) => {
+            console.log('Erro: ' + error)
         })
 }
-document.querySelector('#botao').addEventListener('click', clicou)
